81104eb417dad8f3d6a1270c0f3fc4917b0ff7fd
Pugmeowla <andretnk49@hotmail.com>
2024-03-21 17:54:37 +1100

    fedora in combat tab

--- src/main/java/net/pugmeowla/vertebrata/Vertebrata.java
+++ src/main/java/net/pugmeowla/vertebrata/Vertebrata.java
@@ -1,7 +1,9 @@
 package net.pugmeowla.vertebrata;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.CreativeModeTabRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -37,7 +39,9 @@ public class Vertebrata {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.COMBAT) {
+            event.accept(ModItems.Fedora);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
