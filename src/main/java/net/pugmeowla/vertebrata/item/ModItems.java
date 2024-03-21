a3d794a124fd044799022162343549e9761b6c25
Pugmeowla <andretnk49@hotmail.com>
2024-03-21 17:52:02 +1100

    fedora on moditems thing

--- src/main/java/net/pugmeowla/vertebrata/item/ModItems.java
+++ src/main/java/net/pugmeowla/vertebrata/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.pugmeowla.vertebrata.item;
 
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.RegistryObject;
 import net.pugmeowla.vertebrata.Vertebrata;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
@@ -9,6 +10,9 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Vertebrata.MOD_ID);
+
+    public static final RegistryObject<Item> Fedora = ITEMS.register("fedora",
+        () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
